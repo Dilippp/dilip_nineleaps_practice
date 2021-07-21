27b13b23b95f12a3f288db5338e5afa23e3f24a6
Dilippp <dilip.chauhan@nineleaps.com>
2021-07-21 16:33:08 +0530

    init commit

---
+++ hystrix-service/src/test/java/com/nineleaps/api/hystrixservice/HystrixServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineleaps.api.hystrixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
